6e2002b5fd2bc5479fced9dbaea46341f0a77284
buse2808 <68809716+buse2808@users.noreply.github.com>
2022-01-04 19:33:45 +0300

    s

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
-  firebaseConfig: {
-    apiKey: "AIzaSyCaQ1oBMtWgQx0AmlaX3ONs-YlQJJl6Yv4",
-    authDomain: "busenurhaber.firebaseapp.com",
-    projectId: "busenurhaber",
-    storageBucket: "busenurhaber.appspot.com",
-    messagingSenderId: "1035618150661",
-    appId: "1:1035618150661:web:47adc42465c1084a8c2f6c"
+ firebaseConfig: {
+    apiKey: "**********",
+    authDomain: "**********",
+    projectId: "**********",
+    storageBucket: "**********",
+    messagingSenderId: "**********",
+    appId: "**********"
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyCaQ1oBMtWgQx0AmlaX3ONs-YlQJJl6Yv4",
-    authDomain: "busenurhaber.firebaseapp.com",
-    projectId: "busenurhaber",
-    storageBucket: "busenurhaber.appspot.com",
-    messagingSenderId: "1035618150661",
-    appId: "1:1035618150661:web:47adc42465c1084a8c2f6c"
+    apiKey: "**********",
+    authDomain: "**********",
+    projectId: "**********",
+    storageBucket: "**********",
+    messagingSenderId: "**********",
+    appId: "**********"
   }
 };
 
